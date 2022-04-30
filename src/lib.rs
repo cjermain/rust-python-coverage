391444a043410ac81062c3fe2e650685a5bf2c76
Colin Jermain <cjermain@gmail.com>
2022-04-30 16:17:19 -0400

    Adding Rust-only function with test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 
 use pyo3::prelude::*;
 
+fn diff_as_string(a: i64, b: i64) -> String {
+    (a - b).to_string()
+}
+
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
@@ -12,4 +16,15 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 fn foobar(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_diff_as_string() {
+        assert_eq!(diff_as_string(2, 1), "1");
+        assert_eq!(diff_as_string(1, 2), "-1");
+    }
 }
